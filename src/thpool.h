9c2ea824bcd55a53a1732ac1a05f8b04d05d9ad2
Gary Hsiao <26973371+garyh0205@users.noreply.github.com>
2019-04-12 16:03:49 +0800

    Change slots number by counting the number of threads and prepare the defined number of slots.

--- src/thpool.h
+++ src/thpool.h
@@ -64,7 +64,7 @@
 
 
 /* The number of slots for the memory pool */
-#define SLOTS_FOR_MEM_POOL 1000
+#define SLOTS_FOR_MEM_POOL_PER_THREAD 5
 #define WAITING_TIME 1
 
 #define err(str) fprintf(stderr, str)
